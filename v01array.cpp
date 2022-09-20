afcf0d8642c92092d872d628d80f697e97ff3d1c
domvil <113367128+domvil@users.noreply.github.com>
2022-09-20 18:28:39 +0300

    Update v01array.cpp

--- v01array.cpp
+++ v01array.cpp
@@ -14,30 +14,39 @@ using std::cin;
 struct Studentas{
     string vardas;
     string pavarde;
-    int ndSkaicius;
-    int ndRezultatai[10];
+    int ndSkaicius=0;
+    int *ndRezultatai;
     int egzRez;
     double galutinis;
 };
 
-int random=0;
-string pasirinkimas;
+int random;
+int pasirinkimas;
 
 void intIvedimas(int &priskirti, int lowerBound, int upperBound){
     if(random==0){
     cin >> priskirti;
-    while(cin.fail() || priskirti > upperBound || priskirti < lowerBound){
-        cout << "Bloga ivestis, ivesti sveika skaiciu is [" << lowerBound << " , " << upperBound << "]\n";
+    while(cin.fail() || (priskirti > upperBound && upperBound!=-1) || priskirti < lowerBound){
+        cout << "Bloga ivestis, ivesti sveika skaiciu is [" << lowerBound << " , ";
+        if(upperBound==-1){
+            cout << "int32lim]\n";
+        }
+        else{
+            cout << upperBound << "]\n";
+        }
         cin.clear();
         cin.ignore(80,'\n');
         cin >> priskirti;
     }
     }
     else{
-        priskirti = rand()%upperBound;
+        priskirti = rand()%11;
         cout << "Atsitiktinai priskirta " << priskirti << "\n";
     }
 }
+
+
+
 
 double median(int arr[], int n){
     if(n==0) return 0;
@@ -56,7 +65,8 @@ double avg(int arr[], int n){
     return 0;
 }
 
-void read(Studentas &s){
+void read(Studentas &s, int nr){
+    cout << "Iveskite " << nr+1 << "-tojo studento duomenis:\n";
     cout << "Iveskite studento varda:\n";
     cin >> s.vardas;
     cin.ignore(80,'\n');
@@ -64,25 +74,20 @@ void read(Studentas &s){
     cin >> s.pavarde;
     cin.ignore(80,'\n');
     cout << "Ar studento namu darbus sugeneruoti atsitiktinai ar ivesite patys?\n1 - Atsitiktinai 0 - Pats\n";
+    random = 0;
     intIvedimas(random,0,1);
     cout << "Kiek studentas turejo namu darbu?\n";
-    intIvedimas(s.ndSkaicius,0,10);
+    intIvedimas(s.ndSkaicius,0,-1);
+    s.ndRezultatai = new (std::nothrow) int[s.ndSkaicius];
     for(int i = 0; i < s.ndSkaicius; i++){
         cout << "Iveskite " << i+1 << "-aji namu darbo rezultata: \n";
         intIvedimas(s.ndRezultatai[i],0,10);
     }
+
     cout << "Iveskite studento egzamino rezultata:\n";
     intIvedimas(s.egzRez,0,10);
-    cout << "Ar norite, jog galutiniam bale butu naudojamas namu darbu vidurkis ar mediana?\nIvesti Vidurkis arba Mediana\n";
-    cin >> pasirinkimas;
-    while(pasirinkimas != "Vidurkis" && pasirinkimas != "Mediana"){
-        cout << "Bloga ivestis, ivesti Vidurkis arba Mediana:\n";
-        cin.clear();
-        cin.ignore(80,'\n');
-        cin >> pasirinkimas;
-    }
     double temp;
-    if(pasirinkimas=="Vidurkis"){
+    if(pasirinkimas==1){
         temp = avg(s.ndRezultatai,s.ndSkaicius);
     }
     else temp = median(s.ndRezultatai,s.ndSkaicius);
@@ -91,17 +96,25 @@ void read(Studentas &s){
 }
 
 void print(Studentas s) {
-  printf("%-12s %-12s Galutinis (%s)\n", "Vardas", "Pavarde",pasirinkimas.c_str());
   printf("%-12s %-12s %-16.2lf \n", s.vardas.c_str(),s.pavarde.c_str(), s.galutinis);
 }
 
 int main()
 {
+    int studentu_skaicius;
+    cout << "Iveskite studentu skaiciu:\n";
+    intIvedimas(studentu_skaicius,1,-1);
+    cout << "Ar norite, jog galutiniam bale butu naudojamas namu darbu vidurkis ar mediana?\nIvesti 0 arba 1\n0 - Mediana, 1 - Vidurkis\n";
+    intIvedimas(pasirinkimas,0,-1);
+    Studentas *sar = new Studentas[studentu_skaicius];
+    for(int i= 0; i < studentu_skaicius; i++){
+        read(sar[i],i);
+    }
+    string var[3] = {"Med.", "Vid."};
     srand(time(NULL));
-    Studentas s;
-    read(s);
-    print(s);
+    printf("%-12s %-12s Galutinis (%s)\n", "Vardas", "Pavarde",var[pasirinkimas].c_str());
+    for(int i = 0; i < studentu_skaicius; i++){
+        print(sar[i]);
+    }
     return 0;
 }
-
-
